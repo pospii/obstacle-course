5026137fa4f5c5124e0f8cebe93e539f1d0022b2
Adam Pospíšek <adam.pospisek@email.cz>
2021-12-05 11:02:24 +0100

    added transform of the player

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -7,12 +7,12 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.Translate(1, 0.667f, 20);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0.001f, 0, 0);
     }
 }
