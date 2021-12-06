d7bff7fc904ae63513626d795c2c80bbe00250f2
Adam Pospíšek <adam.pospisek@email.cz>
2021-12-06 18:06:24 +0100

    Serialize Field Variable accessible in the inspector

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    float xValue = 0f;
-    float yValue = 0.01f;
-    float zValue = 0f;
+    [SerializeField] float xValue = 0f;
+    [SerializeField] float yValue = 0.01f;
+    [SerializeField] float zValue = 0f;
 
     
     void Start()
