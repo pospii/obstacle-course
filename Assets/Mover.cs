949e1be62d5d723cba07f9321ff0bf6702b1fa1d
Adam Pospíšek <adam.pospisek@email.cz>
2021-12-06 17:46:46 +0100

    Flying cube xD

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float xValue = 0f;
+    float yValue = 0.01f;
+    float zValue = 0f;
+
+    
     void Start()
     {
-        transform.Translate(1, 0.667f, 20);
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(0.001f, 0, 0);
-    }
+        transform.Translate(xValue, yValue, zValue);
+    } 
 }
