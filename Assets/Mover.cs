984b3d27bb212538308602dc6f458fa4bf28f3e6
Adam Pospíšek <adam.pospisek@email.cz>
2022-02-21 18:48:18 +0100

    create methods

--- Assets/Mover.cs
+++ Assets/Mover.cs
@@ -3,17 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
-{    
+{
     [SerializeField] float moveSpeed = 40f;
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, 0, zValue);
-    } 
+    }
 }
