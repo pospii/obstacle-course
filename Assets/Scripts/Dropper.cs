fdc94d386d18159fe8453ad6bd2cb23c8755647a
Adam Pospíšek <adam.pospisek@email.cz>
2022-03-07 18:41:05 +0100

    create dropper, set timing

---
+++ Assets/Scripts/Dropper.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dropper : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Debug.Log(Time.time);
+    }
+}
