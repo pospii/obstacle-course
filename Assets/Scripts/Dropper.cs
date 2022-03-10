4cca15fe883637a0eee5da0a8cf95bc601006e31
Adam Pospíšek <adam.pospisek@email.cz>
2022-03-10 21:20:17 +0100

    statement afrer few seconds, falling obstacle

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    MeshRenderer renderer;
+    Rigidbody rigidbody;
+    [SerializeField] float timeToWait = 5f;
     void Start()
     {
+        renderer = GetComponent<MeshRenderer>();
+        rigidbody = GetComponent<Rigidbody>();
         
+        renderer.enabled = false;
+        rigidbody.useGravity = false;
     }
 
     void Update()
     {
-        Debug.Log(Time.time);
+        if(Time.time > timeToWait)
+        {
+            Debug.Log("5 seconds has elapsed");
+            renderer.enabled = true;
+            rigidbody.useGravity = true;
+        }
     }
 }
